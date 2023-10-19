dd5d0d276cba20103b43850b1c06177c8cc8917d
c0b20191 <c0b2019122@edu.teu.ac.jp>
2023-10-19 18:28:44 +0900

    repire

--- RoadInfoToCar.cs
+++ RoadInfoToCar.cs
@@ -12,7 +12,7 @@ public class RoadInfoToCar
         this.carInformation = carAgent.carInformation;
     }
 
-    public void RecognizeLane(ref List<float> observations)
+    public void RecognizeLane(ref List<float> observations) //タイルの判別
     {   
         var carCenter = carAgent.transform.position + Vector3.up;
         if (Physics.Raycast(carCenter, Vector3.down, out var hit, 2f))
@@ -20,7 +20,7 @@ public class RoadInfoToCar
             var newHitTile = hit.transform;
 
             // If on MainLane tile
-            if (newHitTile.CompareTag("MainLaneTile"))
+            if (newHitTile.CompareTag("MainLaneTile"))　
             {
                 getCarInfoMergingLane(ref observations);
             }
@@ -31,35 +31,33 @@ public class RoadInfoToCar
             }
             else
             {
-                //observations.Add(0);
-               //observations.Add(0); 
-                //observations.Add(0);
+                observations.Add(0);
+                observations.Add(0); 
+                observations.Add(0);
             }
         }
         else
         {
-            //observations.Add(0);
-           // observations.Add(0); 
-            //observations.Add(0);
+            observations.Add(0);
+            observations.Add(0); 
+            observations.Add(0);
         }
     }
-    public void getCarInfoMineLane(ref List<float> observations)
+    public void getCarInfoMineLane(ref List<float> observations)//MineLaneTileの上にいる一番近い車の位置の取得
     {
-        // MergingLaneのタイル位置とサイズを取得する必要があります。
-        // この例では、MergingLaneの中心位置とサイズを事前に知っていると仮定しています。
         Vector3 mergingLaneCenter = new Vector3(-10f, 0f, -15f);
         Vector3 mergingLaneSize = new Vector3(10f, 10f, 10f);
         
         Collider[] hitColliders = Physics.OverlapBox(mergingLaneCenter, mergingLaneSize * 0.5f);
 
-        /*if(hitColliders.Length > 0)
+        if(hitColliders.Length > 0)
         {
             observations.Add(1);
         }
         else
         {
             observations.Add(0);
-        }*/
+        }
 
         float closestDistance = float.MaxValue;
         Vector3 closestCarPosition = Vector3.zero;
@@ -77,31 +75,25 @@ public class RoadInfoToCar
                 }
             }
         }
-        //observations.Add(closestCarPosition.x);
-        //observations.Add(closestCarPosition.z);
-
-        // ここでclosestCarPositionは、MergingLane上で自分の車に最も近い車の位置です。
-        // 必要に応じてこの情報を使用します。
+        observations.Add(closestCarPosition.x);
+        observations.Add(closestCarPosition.z);
     }
     
-    public void getCarInfoMergingLane(ref List<float> observations)
+    public void getCarInfoMergingLane(ref List<float> observations) //MergingLaneTileの上にいる一番近い車の位置の取得
     {
-        // MainLaneのタイル位置とサイズを取得する必要があります。
-        // この例では、MainLaneの中心位置とサイズを事前に知っていると仮定しています。
         Vector3 mainLaneCenter = new Vector3(0f, 0f, -25f);
-        Vector3 mainLaneSize = new Vector3(10f, 10f, 10f); // TODO: 実際のサイズを設定
-        // MainLaneの位置でBoxをオーバーラップして、そこにあるすべてのコライダーを取得
-        Collider[] hitColliders = Physics.OverlapBox(mainLaneCenter, mainLaneSize * 0.5f);
-        Debug.Log("MineLaneInfoGet");
+        Vector3 mainLaneSize = new Vector3(10f, 10f, 10f); 
 
-        /*if(hitColliders.Length > 0)
+        Collider[] hitColliders = Physics.OverlapBox(mainLaneCenter, mainLaneSize * 0.5f);
+
+        if(hitColliders.Length > 0)
         {
             observations.Add(1);
         }
         else
         {
             observations.Add(0);
-        }*/
+        }
         
         float closestDistance = float.MaxValue;
         Vector3 closestCarPosition = Vector3.zero;
@@ -119,10 +111,8 @@ public class RoadInfoToCar
                 }
             }
         }
-        //observations.Add(closestCarPosition.x);
-        //observations.Add(closestCarPosition.z);
-    // ここでclosestCarPositionは、MainLane上で自分の車に最も近い車の位置です。
-    // 必要に応じてこの情報を使用します。
+        observations.Add(closestCarPosition.x);
+        observations.Add(closestCarPosition.z);
     }
 
 }
